777aa2906501c7f92017a8ae746cff912952e1ff
lifeofstephanie <stephanieanyanwu605@gmail.com>
2023-11-28 07:05:50 -0800

    wow

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for c in range (ord('a'), ord('z') + 1):
-    if chr(c) != 'e' and chr(c) != 'q':
-        print("{:c}".format(c), end="")
+    if c == ord('q') or c == ord('e'):
+        continue
+    print("{:c}".format(c), end="")
