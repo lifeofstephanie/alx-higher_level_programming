2367f3d84f6698217ed6d091bd339b83aa5aa23d
lifeofstephanie <stephanieanyanwu605@gmail.com>
2023-11-28 08:00:22 -0800

    l

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+
+
 def remove_char_at(str, n):
-    if 0 <= n < len(str):
-        return(str.replace(str[n], ''))
-    else:
-        return(str)
+    if n >= 0 and n < len(str):
+        return str[:n] + str[n+1:]
+    return str[:]
