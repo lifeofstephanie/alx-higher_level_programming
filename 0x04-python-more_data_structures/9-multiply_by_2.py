7a9283f69774c43506866a37e1ef6028e980e3f1
lifeofstephanie <stephanieanyanwu605@gmail.com>
2023-12-07 11:38:13 -0800

    lol

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    return {k: v * 2 for k, v in a_dictionary.items()}
+    new = {}
+    for i in a_dictionary:
+        new[i] = a_dictionary[i] * 2
+    return new
